e736dad6264e0e533a261c7e62cbbe073f715df5
224368 Makrocki Robert <224368@edu.p.lodz.pl>
2022-01-24 02:23:21 +0100

    [ADDED] Dialog popup with exit app confirmation

--- lib/layout.dart
+++ lib/layout.dart
@@ -6,24 +6,53 @@ import 'package:perfectBeta/widgets/side_menu.dart';
 
 import 'widgets/top_nav.dart';
 
-class SiteLayout extends StatelessWidget {
+class SiteLayout extends StatefulWidget {
+  @override
+  State<SiteLayout> createState() => _SiteLayoutState();
+}
+
+class _SiteLayoutState extends State<SiteLayout> {
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      extendBodyBehindAppBar: true,
-      appBar: topNavigationBar(context, scaffoldKey),
-      drawer: Drawer(
-        child: SideMenu(),
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        key: scaffoldKey,
+        extendBodyBehindAppBar: true,
+        appBar: topNavigationBar(context, scaffoldKey),
+        drawer: Drawer(
+          child: SideMenu(),
+        ),
+        body: ResponsiveWidget(
+            largeScreen: LargeScreen(),
+            hugeScreen: LargeScreen(),
+            smallScreen: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: localNavigator(),
+            )),
       ),
-      body: ResponsiveWidget(
-          largeScreen: LargeScreen(),
-          hugeScreen: LargeScreen(),
-          smallScreen: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: localNavigator(),
-          )),
     );
+  }
+
+  Future<bool> _onWillPop() async {
+    return (await showDialog(
+      context: context,
+      builder: (context) => new AlertDialog(
+        title: new Text('Are you sure?'),
+        content: new Text('Do you want to exit an App'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: new Text('No'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(true),
+            child: new Text('Yes'),
+          ),
+        ],
+      ),
+    )) ?? false;
   }
 }
