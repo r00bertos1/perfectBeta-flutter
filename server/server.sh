ed719d06756a066348e7eb384d50b63b53ca6947
224368 Makrocki Robert <224368@edu.p.lodz.pl>
2022-01-28 16:18:10 +0100

    [MODIFIED] Dockerfile and server.sh from CRLF to LF

--- server/server.sh
+++ server/server.sh
@@ -4,11 +4,11 @@
 echo 'Server start script initialized...'
 
 # Set the port
-PORT=4040
+PORT=2137
 
 # Kill anything that is already running on that port
 echo 'Cleaning port' $PORT '...'
-fuser -k 4040/tcp
+fuser -k 2137/tcp
 
 # Change directories to the release folder
 cd build/web/
